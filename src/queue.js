d86070554131f1a24fbfe6e3ecddefd438db3d2d
pokolga <pokolga1801@gmail.com>
2022-05-02 12:25:47 +0300

    init

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,22 +12,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.enqueue(3); // adds the element to the queue
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
-class Queue {
+ Каждый экземпляр очереди должен иметь 3 метода:
+    * `enqueue(value)` — помещает `value` в конец **очереди** 
+    * `deque` — извлекает значение с начала **очереди** и удаляет его
+    * `getUnderlyingList` - возвращает **связный список**, лежащий в основе данной **очереди**
 
+ */
+
+class Queue {
+    constructor(value){
+        super();
+        this.value = value;
+    }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    alert("getUnderlyingList")
   }
 
   enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    alert("enqueue")
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      alert("dequeue")
   }
 }
 
